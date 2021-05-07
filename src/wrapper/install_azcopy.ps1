1791c7402fd75ad55aafdd351f4654aa5a648c7c
Major Pain <gary_baird@msn.com>
2021-05-07 15:16:47 -0600

    Create install_azcopy.ps1

---
+++ src/wrapper/install_azcopy.ps1
@@ -0,0 +1,3 @@
+# TBD: Onload, get OS and then pick installer automatically.
+# Possible version support
+# Linux/Windows/OSX etx functions for each along with automatically pulling dependencies per linux distro.
