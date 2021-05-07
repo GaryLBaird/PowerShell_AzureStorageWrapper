4221777ec786d1a1d1889c18e15ebbee32068955
Major Pain <gary_baird@msn.com>
2021-05-07 15:07:52 -0600

    Delete securitysettings.ps1

--- src/authentication/securitysettings.ps1
+++
@@ -1,3 +0,0 @@
-# TBD: Azure Storage Connection Settings Lookup
-# Need to lookup UserConnection system runtime variables for automatied systems
-# Needs to support read encrypted security principles from multiple systems including databags and secrets.
