f1a4c344c94d58d427070b9a4ff6716580409d2a
Major Pain <gary_baird@msn.com>
2021-05-07 15:14:15 -0600

    Adding default wapper TBD's

---
+++ src/wrapper/azcopy_wrap.ps1
@@ -0,0 +1,21 @@
+# TBD: Check for azcopy utility and instll it if missing, error on failure.
+# TBD: Support the following functions:
+## Command	Description
+## azcopy bench	- Runs a performance benchmark by uploading or downloading test data to or from a specified location.
+## azcopy copy	- Copies source data to a destination location
+## azcopy doc	- Generates documentation for the tool in Markdown format.
+## azcopy env	- Shows the environment variables that can configure AzCopy's behavior.
+## azcopy jobs	- Subcommands related to managing jobs.
+## azcopy jobs - clean	Remove all log and plan files for all jobs.
+## azcopy jobs - list	Displays information on all jobs.
+## azcopy jobs - remove	Remove all files associated with the given job ID.
+## azcopy jobs - resume	Resumes the existing job with the given job ID.
+## azcopy jobs - show	Shows detailed information for the given job ID.
+## azcopy load	- Subcommands related to transferring data in specific formats.
+## azcopy load clfs	- Transfers local data into a Container and stores it in Microsoft's Avere Cloud FileSystem (CLFS) format.
+## azcopy list	- Lists the entities in a given resource.
+## azcopy login	- Logs in to Azure Active Directory to access Azure Storage resources.
+## azcopy logout	- Logs the user out and terminates access to Azure Storage resources.
+## azcopy make	- Creates a container or file share.
+## azcopy remove	- Delete blobs or files from an Azure storage account.
+## azcopy sync	- Replicates the source location to the destination location.
