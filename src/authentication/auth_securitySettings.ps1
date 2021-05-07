b1559523214da6a9de4a12ec94311ea3b0d09b37
Major Pain <gary_baird@msn.com>
2021-05-07 15:08:22 -0600

    Create auth_securitySettings.ps1

---
+++ src/authentication/auth_securitySettings.ps1
@@ -0,0 +1,3 @@
+# TBD: Azure Storage Connection Settings Lookup
+# Need to lookup UserConnection system runtime variables for automatied systems
+# Needs to support read encrypted security principles from multiple systems including databags and secrets.
